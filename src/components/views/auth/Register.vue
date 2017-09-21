0620edd12a020a177604fe3d48487bd22543b315
MuhammadHamdi1 <prime.energy7@gmail.com>
2017-09-21 22:38:19 +0200

    WIP

--- src/components/views/auth/Register.vue
+++ src/components/views/auth/Register.vue
@@ -11,36 +11,39 @@
                         <i class="fa fa-user-circle fa-5x"></i>
                     </div>
                     <br>
+                    <div class="alert alert-danger" v-if="error">
+                        <p><strong>Passwords</strong> don't Match!</p>
+                    </div>
                     <br>
-                    <form class="form-horizontal">
+                    <form class="form-horizontal" @submit.prevent="register">
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="email"><i class="fa fa-envelope-o fa-2x"></i></label>
                         <div class="col-sm-10">
-                        <input type="email" class="form-control" id="email" placeholder="Enter email">
+                        <input type="email" class="form-control" id="email" placeholder="Enter email" v-model="user.email">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="pwd"><i class="fa fa-lock fa-2x"></i></label>
                         <div class="col-sm-10"> 
-                        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" v-model="user.password">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="pwd2"><i class="fa fa-unlock-alt fa-2x"></i></label>
                         <div class="col-sm-10"> 
-                        <input type="password" class="form-control" id="pwd2" placeholder="Confirm password">
+                        <input type="password" class="form-control" id="pwd2" placeholder="Confirm password" v-model="pwdConfirm">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="user"><i class="fa fa-user fa-2x"></i></label>
                         <div class="col-sm-10"> 
-                        <input type="text" class="form-control" id="user" placeholder="Username">
+                        <input type="text" class="form-control" id="user" placeholder="Username" v-model="user.username">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-2" for="name"><i class="fa fa-user-o fa-2x"></i></label>
                         <div class="col-sm-10"> 
-                        <input type="text" class="form-control" id="name" placeholder="Name">
+                        <input type="text" class="form-control" id="name" placeholder="Name" v-model="user.name">
                         </div>
                     </div>
                     <div class="form-group"> 
@@ -52,7 +55,7 @@
                     </div>
                     <div class="form-group"> 
                         <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary">Login</button>
+                        <button type="submit" class="btn btn-primary">Register</button>
                         </div>
                     </div>
                     </form>
@@ -64,7 +67,43 @@
 </template>
 
 <script>
-    
+    import {api} from '../../../config/axios'
+    export default {
+        data() {
+            return {
+                user: {
+                    name: '',
+                    email: '',
+                    username: '',
+                    password: ''
+                },
+                pwdConfirm: '',
+                error: false
+            }
+        },
+        created() {
+            var token = localStorage.getItem('token');
+            if (token) {
+                this.$router.push('/')
+            }
+        },
+        methods: {
+            register(){
+                if (this.user.password != this.pwdConfirm) {
+                    this.error = true;
+                } else {
+                    api.post('register', this.user)
+                    .then((res) => {
+                        localStorage.setItem('token', res.token);
+                        localStorage.setItem('user_id', res.user_id);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+                }
+            }
+        }
+    }
 </script>
 
 <style scoped>
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import config from './config';
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: `${config.url}/api`,
     headers: {
         'x-access-token': localStorage.getItem('token') || '',
     },
-})
-
-window.api = api;
+});
